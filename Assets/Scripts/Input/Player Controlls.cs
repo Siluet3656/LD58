d170785ef48ca6b7880701ee775a57baae2971e7
Siluet3656 <Mingaleev02@mail.ru>
2025-10-03 22:54:43 +0400

    basic setup

---
+++ Assets/Scripts/Input/Player Controlls.cs
@@ -0,0 +1,67 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Input/Player Controlls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+namespace Input
+{
+    public class @PlayerControlls : IInputActionCollection, IDisposable
+    {
+        public InputActionAsset asset { get; }
+        public @PlayerControlls()
+        {
+            asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Player Controlls"",
+    ""maps"": [],
+    ""controlSchemes"": []
+}");
+        }
+
+        public void Dispose()
+        {
+            UnityEngine.Object.Destroy(asset);
+        }
+
+        public InputBinding? bindingMask
+        {
+            get => asset.bindingMask;
+            set => asset.bindingMask = value;
+        }
+
+        public ReadOnlyArray<InputDevice>? devices
+        {
+            get => asset.devices;
+            set => asset.devices = value;
+        }
+
+        public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+        public bool Contains(InputAction action)
+        {
+            return asset.Contains(action);
+        }
+
+        public IEnumerator<InputAction> GetEnumerator()
+        {
+            return asset.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Enable()
+        {
+            asset.Enable();
+        }
+
+        public void Disable()
+        {
+            asset.Disable();
+        }
+    }
+}
