c1954b6e24471a418765113756089261cd4a713f
Siluet3656 <Mingaleev02@mail.ru>
2025-10-05 03:32:26 +0400

    New UI ++ polishing

---
+++ Assets/Scripts/Effects/ColorCycler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Effects
+{
+    public class ColorCycler : MonoBehaviour
+    {
+        [SerializeField] private Color _color1 = Color.red;
+        [SerializeField] private Color _color2 = Color.blue;
+        [SerializeField] private float _duration = 2.0f;
+
+        private Text _text;
+        private float _timeElapsed;
+
+        private void Awake()
+        {
+            _text = GetComponent<Text>();
+            _timeElapsed = 0f;
+            if (_text == null)
+            {
+                Debug.LogError("Text component not found!");
+                enabled = false;
+            }
+        }
+
+        private void Update()
+        {
+            float t = Mathf.PingPong(_timeElapsed / _duration, 1f);
+            _text.color = Color.Lerp(_color1, _color2, t);
+            _timeElapsed += Time.deltaTime;
+        }
+    }
+}
