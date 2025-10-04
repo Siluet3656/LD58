a6bb0a2b4e3f2541981ee43ab768c80028800bd4
Siluet3656 <Mingaleev02@mail.ru>
2025-10-04 10:44:14 +0400

    Fight scene + hp

---
+++ Assets/Scripts/EntityResources/Hp.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using View;
+
+namespace EntityResources
+{
+    [RequireComponent(typeof(HpView))]
+    public class Hp : MonoBehaviour
+    {
+        [Header("Base Stats")]
+        [SerializeField, Min(1)] private int _maxHealth;
+
+        private float _currentHealth;
+        private float _additionalHealth;
+        private int _shieldStacks;
+        private bool _isInvulnerable;
+        
+        private void Awake()
+        {
+            InitializeHealth();
+        }
+
+        private void TakeDamage(float damage)
+        {
+            if (_isInvulnerable) return;
+            
+            ApplyDamageToHealth(damage);
+            //DamagePopup.Instance.AddText((int)damage, transform.position);
+        }
+
+        private void ApplyDamageToHealth(float damage)
+        {
+            _currentHealth = Mathf.Max(0, _currentHealth - damage);
+            OnHealthChanged?.Invoke(_currentHealth);
+            
+            if (_currentHealth <= 0)
+                Die();
+        }
+
+        private void Die()
+        {
+            OnDeath?.Invoke();
+        }
+        
+        public event Action<float> OnHealthChanged;
+        public event Action OnDeath;
+        public event Action OnCriticalDamageReceived; 
+        public event Action<float> OnAnyDamageReceived; 
+
+        public bool IsInvulnerable => _isInvulnerable;
+        public float MaxHealth => _maxHealth;
+        public float CurrentHealth => _currentHealth; 
+        
+        public void InitializeHealth()
+        {
+            _currentHealth = Mathf.Max(_maxHealth, 1);
+            OnHealthChanged?.Invoke(_currentHealth);
+        }
+
+        public void SetMaxHealth(int health)
+        {
+            _maxHealth = health;
+        }
+
+        public void TryToTakeCriticalDamage(float damage, float criticalMultiply, float criticalChance)
+        {
+            if (_isInvulnerable) return;
+            
+            bool isCritical = Random.Range(0f, 1f) < criticalChance;
+            float finalDamage = isCritical ? damage * criticalMultiply : damage;
+            
+            if (isCritical) OnCriticalDamageReceived?.Invoke();
+
+            OnAnyDamageReceived?.Invoke(finalDamage);
+            
+            TakeDamage(finalDamage); Debug.Log($"Damage taken by {gameObject}: {finalDamage}");
+        }
+
+        public void TryToTakeDamage(float damage, bool isDamageAdditional)
+        {
+            if (_isInvulnerable) return;
+            
+            if (isDamageAdditional == false) OnAnyDamageReceived?.Invoke(damage);
+                
+            TakeDamage(damage); Debug.Log($"Damage taken by {gameObject}: {damage}");
+        }
+
+        public void GetInvulnerable()
+        {
+            _isInvulnerable = true;
+        }
+        
+        public void GetVulnerable()
+        {
+            _isInvulnerable = false;
+        }
+
+        public void Heal(float healAmount)
+        {
+            if (healAmount <= 0) return;
+            
+            float resultHeal = Mathf.Min(_currentHealth + healAmount, _maxHealth); Debug.Log($"Heal taken by {gameObject}: {resultHeal - _currentHealth}");
+            _currentHealth = resultHeal;
+            OnHealthChanged?.Invoke(_currentHealth);
+        }
+    }
+}
---
+++ Assets/Scripts/View/HpView.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using EntityResources;
+
+namespace View
+{
+    public class HpView : MonoBehaviour
+    {
+        [Header("UI References")]
+        [SerializeField] private Image _healthBar;
+
+        private Hp _hp;
+
+        private void Awake()
+        {
+            _hp = GetComponent<Hp>();
+        }
+
+        private void OnEnable()
+        {
+            SubscribeToEvents();
+        }
+
+        private void OnDisable()
+        {
+            UnsubscribeFromEvents();
+        }
+
+        private void SubscribeToEvents()
+        {
+            if (_hp == null) return;
+            
+            _hp.OnHealthChanged += UpdateHealthBar;
+        }
+
+        private void UnsubscribeFromEvents()
+        {
+            if (_hp == null) return;
+            
+            _hp.OnHealthChanged -= UpdateHealthBar;
+        }
+
+        private void UpdateHealthBar(float currentHealth)
+        {
+            if (_hp == null || _healthBar == null) return;
+            
+            _healthBar.fillAmount = currentHealth / _hp.MaxHealth;
+        }
+    }
+}
