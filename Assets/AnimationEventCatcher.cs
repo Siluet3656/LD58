5bfd26a86606ea0e9adc08cde143944bdefddd05
Siluet3656 <Mingaleev02@mail.ru>
2025-10-05 14:06:16 +0400

    Enemy death animation

--- Assets/AnimationEventCatcher.cs
+++ Assets/AnimationEventCatcher.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 public class AnimationEventCatcher : MonoBehaviour
 {
     public Action OnAttackEnd;
+    public Action OnDeathAnimEnd;
     public void CatchAttackEnd()
     {
         OnAttackEnd?.Invoke();
+    }
+
+    public void CatchDeathAnimEnd()
+    {
+        OnDeathAnimEnd?.Invoke();
     }
 }
--- Assets/Scripts/View/EnemyView.cs
+++ Assets/Scripts/View/EnemyView.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using EntityResources;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace View
@@ -7,11 +8,13 @@ namespace View
     {
         private static readonly int AttackEnd = Animator.StringToHash("AttackEnd");
         private static readonly int Attack = Animator.StringToHash("Attack");
+        private static readonly int Death = Animator.StringToHash("Death");
         [SerializeField] private Image _swingBar;
         [SerializeField] private Text _textDamage;
         
         [SerializeField] private Animator _animator;
         [SerializeField] private AnimationEventCatcher _animationEventCatcher;
+        [SerializeField] private Hp _myHp;
         
         private void SetTrigger()
         {
@@ -21,6 +24,8 @@ namespace View
         private void OnEnable()
         {
             _animationEventCatcher.OnAttackEnd += SetTrigger;
+            _animationEventCatcher.OnDeathAnimEnd += () => gameObject.SetActive(false);
+            _myHp.OnDeath += () => _animator.SetTrigger(Death);
         }
 
         private void OnDisable()
