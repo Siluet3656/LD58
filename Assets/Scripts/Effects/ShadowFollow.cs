44a945c69dd36ff4a263b406005e3ab64c674eab
Siluet3656 <Mingaleev02@mail.ru>
2025-10-05 07:15:52 +0400

    Idle animations

---
+++ Assets/Scripts/Effects/ShadowFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Effects
+{
+    public class ShadowFollow : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+
+        private void Update()
+        {
+            if (_target != null)
+                transform.position = new Vector3(_target.position.x, transform.position.y, transform.position.z);
+        }
+    }
+}
